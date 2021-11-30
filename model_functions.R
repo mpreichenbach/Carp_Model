8f3d517a36b255b4a66a4095b78dca49481c821d
mpreichenbach <matthew.reichenbach@gmail.com>
2021-11-30 11:13:10 -0700

    Added library and commented out diel section

--- model_functions.R
+++ model_functions.R
@@ -11,6 +11,7 @@ library(raster)
 library(rgdal)
 library(sf)
 library(sp)
+library(StreamMetabolism)
 library(tidyverse)
 library(viridis)
 
@@ -286,46 +287,36 @@ treatment.key <- function(trial, pond){
 #             save(ModDat, file=paste0("~/Carp-Model/Fitted CRWs/Trial ", trial, "/", file0, "/", file1))
 #         }
 #     }
+
+# trials <- c(1, 2, 3, 4, 5)
+# path <- '~/Carp-Model/Fitted CRWs with dB'
+# for (trial in trials){
+#     subfolders <- list.files(file.path(path, paste0('Trial ', trial)))
+#     for (folder in subfolders){
+#         files <- list.files(file.path(path, paste0('Trial ', trial), folder))
+#         for (file in files){
+#             print(file)
+#             pond <- substr(file, 18, 19)
+#             load(file.path(path, paste0('Trial ', trial), folder, file))
+#             ModDat$Diel <- NA
+# 
+#             # diel info; 1 is day, 0 is night
+#             SunRS <- sunrise.set(38.9122061924, -92.2795993947,
+#                                  paste0(year(min(ModDat$Time)), '/', month(min(ModDat$Time)), '/',
+#                                         day(min(ModDat$Time))-1), num.days = 8,
+#                                  timezone='America/North_Dakota/Center')
+#             SunRS[,1] <- as.POSIXct(SunRS[,1], origin="1970-01-01", tz = "America/Chicago")
+#             SunRS[,2] <- as.POSIXct(SunRS[,2], origin="1970-01-01", tz = "America/Chicago")
+#             SunRS$Date <- as.Date(SunRS[,1])
+# 
+#             # Day is 0, night is 1
+#             for(j in 1:(nrow(SunRS)-1)){
+#                 ModDat$Diel[(ModDat$Time >= SunRS$sunrise[j]) & (ModDat$Time < SunRS$sunset[j])] <- 1
+#             }
+#             ModDat$Diel[is.na(ModDat$Diel)] <- 0
+# 
+#             save(ModDat, file = file.path(path, paste0('Trial ', trial), folder, file))
+#         }
+#     }
 # }
-
-trials <- c(1, 2, 3, 4, 5)
-path <- '~/Carp-Model/Fitted CRWs with dB'
-temp_data <- temperature.data()
-for (trial in trials){
-    subfolders <- list.files(file.path(path, paste0('Trial ', trial)))
-    for (folder in subfolders){
-        files <- list.files(file.path(path, paste0('Trial ', trial), folder))
-        for (file in files){
-            print(file)
-            pond <- substr(file, 18, 19)
-            load(file.path(path, paste0('Trial ', trial), folder, file))
-            ModDat$Diel <- NA
-            ModDat$Temp <- NA
-            ModDat$DT <- NULL
-            ModDat$TimeNum <- NULL
-            ModDat$time <- NULL
-            ModDat$DT <- NULL
-
-            # diel info; 1 is day, 0 is night
-            SunRS <- sunrise.set(38.9122061924, -92.2795993947,
-                                 paste0(year(min(ModDat$Time)), '/', month(min(ModDat$Time)), '/', 
-                                        day(min(ModDat$Time))-1), num.days = 8, 
-                                 timezone='America/North_Dakota/Center')
-            SunRS[,1] <- as.POSIXct(SunRS[,1], origin="1970-01-01", tz = "America/Chicago")
-            SunRS[,2] <- as.POSIXct(SunRS[,2], origin="1970-01-01", tz = "America/Chicago")
-            SunRS$Date <- as.Date(SunRS[,1])
-            
-            # Day is 0, night is 1
-            for(j in 1:(nrow(SunRS)-1)){
-                ModDat$Diel[(ModDat$Time >= SunRS$sunrise[j]) & (ModDat$Time < SunRS$sunset[j])] <- 1
-            }
-            ModDat$Diel[is.na(ModDat$Diel)] <- 0
-            
-            # temperature info
-            avg_temp <- mean(subset(temp_data, Trial == trial & Pond == pond)$Temp_C)
-            ModDat$Temp[ModDat$Trial == trial & ModDat$Pond == pond] <- avg_temp
-            
-            save(ModDat, file = file.path(path, paste0('Trial ', trial), folder, file))
-        }
-    }
-}
+# }
